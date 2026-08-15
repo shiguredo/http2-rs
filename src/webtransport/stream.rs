6f5341ef5fd09daf214da424fea627c20b1e1221
V <nakai@shiguredo.jp>
2026-08-16 00:00:33 +0900

    0104 未使用の send_max() getter を削除する (#89)
    * 0104 未使用の send_max() getter を削除する
    
    * 0104 解決方法を追記する
    
    * 0104 closed 未使用の send_max() getter を削除する

--- src/webtransport/stream.rs
+++ src/webtransport/stream.rs
@@ -316,12 +316,6 @@ impl WtStream {
         self.recv_max.saturating_sub(self.recv_offset)
     }
 
-    /// 送信上限 (ピアが許可した最大バイト数) を取得する
-    #[must_use]
-    pub const fn send_max(&self) -> u64 {
-        self.send_max
-    }
-
     /// 受信上限 (ローカルが許可した最大バイト数) を取得する
     #[must_use]
     pub const fn recv_max(&self) -> u64 {
